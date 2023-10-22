e8b2e39aea6098100d725bcf9a54817ffb4d5d7f
Marjan <97312708+Marjanamiiri@users.noreply.github.com>
2023-10-22 11:57:44 -0700

    header fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { Header } from "./components/Header";
 import { Assignments } from "./components/Assignments";
 
 function App() {
+  const onClickButton = () => {
+    // Define the behavior when the "Create" button is clicked
+    // You can implement your logic here
+  };
+
+  const onClickInput = () => {
+    // Define the behavior when the input field is clicked
+    // You can implement your logic here
+  };
   return (
     <>
-      <Header />
+      <Header inputText="" onClickButton={onClickButton} onClickInput={onClickInput}/>
       <Assignments />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,33 @@
+import React, { useState } from 'react';
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
-import React, { useState } from 'react'; // Import React and useState
 
-export function Header() {
-  const [inputText, setInputText] = useState(''); // State to store the input text
+type Props = {
+  inputText: string;
+  onClickInput: () => void;
+  onClickButton: () => void;
+}
 
-  // Function to handle input changes
-  const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setInputText(event.target.value);
-  };
-
-  // Function to create a new assignment
-  const createAssignment = () => {
-    if (inputText.trim() !== '') { // Check if inputText is not empty (ignoring spaces)
-      // You can add logic here to create a new assignment using the inputText.
-      // For this example, we'll just clear the input field.
-      setInputText('');
-    }
+export function Header({ onClickInput, onClickButton, inputText }: Props) {
+  const [inputValue, setInputValue] = useState(inputText);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <header className={styles.header}>
+      {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
         <input
           placeholder="Add a new assignment"
           type="text"
-          value={inputText}
+          value={inputValue}
+          onClick={onClickInput}
           onChange={handleInputChange}
         />
-        <button
-          onClick={createAssignment}
-          disabled={inputText.trim() === ''} // Disable the button when inputText is empty (ignoring spaces)
-          style={{ cursor: inputText.trim() === '' ? 'not-allowed' : 'pointer' }} // Change cursor style
-        >
+        <button onClick={onClickButton} disabled={!inputValue.trim().length}>
           Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
