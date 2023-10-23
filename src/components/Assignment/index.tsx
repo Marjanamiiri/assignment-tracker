ec1863468451840016f291c361407a3dae26ed88
Marjan <97312708+Marjanamiiri@users.noreply.github.com>
2023-10-22 17:26:51 -0700

    add

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -3,17 +3,21 @@ import { TbTrash } from "react-icons/tb";
 
 type Props = {
   title: string;
+  onClickConatanier: () => void;
+  onDeleteButton: () => void;
 }
-export function Assignment({ title }: Props) {
+export function Assignment({ title, onClickConatanier, onDeleteButton }: Props) {
   return (
     <div className={styles.assignment}>
-      <button className={styles.checkContainer}>
+      <button className={styles.checkContainer}
+              onClick={onClickConatanier}>
         <div />
       </button>
 
       <p>{title}</p>
 
-      <button className={styles.deleteButton}>
+      <button className={styles.deleteButton}
+              onClick={onDeleteButton}>
         <TbTrash size={20} />
       </button>
     </div>
--- src/components/Assignments/index.tsx
+++ src/components/Assignments/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Assignment } from "../Assignment";
 import styles from "./assignments.module.css";
 
@@ -6,23 +7,45 @@ type Props = {
 }
 
 export function Assignments({ assignments }: Props) {
+  const [hiddenAssignments, setHiddenAssignments] = useState<boolean[]>(Array(assignments.length).fill(false));
+
+  const onClickConatanier = (index: number) => {
+    // Toggle the checked status of the assignment by index
+    const newHiddenAssignments = [...hiddenAssignments];
+    newHiddenAssignments[index] = !newHiddenAssignments[index];
+    setHiddenAssignments(newHiddenAssignments);
+  };
+
+  const onDeleteButton = (index: number) => {
+    // Hide the assignment by index
+    const newHiddenAssignments = [...hiddenAssignments];
+    newHiddenAssignments[index] = true;
+    setHiddenAssignments(newHiddenAssignments);
+  };
+
   return (
     <section className={styles.assignments}>
       <header className={styles.header}>
-      <div>
+        <div>
           <p>Created Assignments</p>
           <span>{assignments.length}</span>
         </div>
 
         <div>
           <p className={styles.textPurple}>Completed Assignments</p>
-          <span>1 of {assignments.length}</span>
+          <span>{} of {assignments.length}</span>
         </div>
       </header>
 
       <div className={styles.list}>
         {assignments.map((assignment, index) => (
-          <Assignment key={index} title={assignment} />
+          <Assignment
+            key={index}
+            title={assignment}
+            onClickConatanier={() => onClickConatanier(index)}
+            onDeleteButton={() => onDeleteButton(index)}
+            
+          />
         ))}
       </div>
     </section>
