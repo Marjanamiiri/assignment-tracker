f5231fc993f785e813a18c04f794443049874562
Marjan <97312708+Marjanamiiri@users.noreply.github.com>
2023-10-31 13:44:57 -0700

    date added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 type Props = {
-
   onClickInput: () => void;
-  onCreateAssignment: (newAssignment: any) => void;
+  onCreateAssignment: (newAssignment: any, deadline: Date) => void;
+  onSelectDate: (date: Date | undefined) => void;
 }
 
-export function Header({ onClickInput, onCreateAssignment }: Props) {
+export function Header({ onClickInput, onCreateAssignment, onSelectDate }: Props) {
   const [inputValue, setInputValue] = useState('');
+  const [selected, setSelected] = useState<Date | undefined>();
+  const [showCalendar, setShowCalendar] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -21,8 +26,18 @@ export function Header({ onClickInput, onCreateAssignment }: Props) {
       // Call the function passed from the parent component to create a new assignment
       onCreateAssignment(inputValue);
       setInputValue(''); // Clear the input field
+      setSelected(undefined);
     }
   };
+
+  const openCalendar = () => {
+    setShowCalendar(true);
+  };
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
 
   return (
     <header className={styles.header}>
@@ -36,6 +51,23 @@ export function Header({ onClickInput, onCreateAssignment }: Props) {
           onClick={onClickInput}
           onChange={handleInputChange}
         />
+        {/* Date is here */}
+        <button onClick={openCalendar}>Select Date</button>
+        {showCalendar && (
+          <div className={styles.calendarContainer}>
+            <DayPicker
+              mode="single"
+              selected={selected}
+              onSelect={date => {
+                setSelected(date);
+                onSelectDate(date);
+                setShowCalendar(false);
+              }}
+              footer={selected ? <p>You picked {format(selected, 'PP')}.</p> : <p>Please pick a day.</p>}
+            />
+          </div>
+        )}
+
         <button onClick={handleCreateAssignment} disabled={!inputValue.trim().length}>
           Create <AiOutlinePlusCircle size={20} />
         </button>
